ae6960bff62fcf77fc5381c521f0a0d9fa9c271d
msi <mattsi2@outlook.com>
2023-10-15 09:09:19 +0800

    Lab1, Task1, Part3 still under building

--- lab1/playground.py
+++ lab1/playground.py
@@ -1,16 +1,16 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
+from numpy import linalg as LA
 
 
-p1 = np.array([0,1,0])
-p2 = np.array([0,1,0])
+p1 = np.array([0.173241,-0.173241,0])
+p2 = np.array([0.245000 ,0,0])
 
-u = p1/np.linalg.norm(p1)
-v = p2/np.linalg.norm(p2)
+r = R.from_euler('XYZ', [0,0,-45], degrees=True)
+print(r.as_matrix().dot(p2))
 
-axis = np.cross(u,v)
-angle = np.arccos(np.dot(u,v))
+print(r.as_matrix())
+print(r.as_quat())
+print(r.as_rotvec())
 
-q = R.from_rotvec(angle * axis)
 
-print(q.as_quat())
